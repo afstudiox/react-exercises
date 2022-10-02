5236957178d8743b6e57cc8a6e32a9f7d048658c
andre <andre@andres-MacBook-Pro.local>
2022-10-02 19:31:32 -0300

    tutorial tania rascia indoducao

---
+++ src/TableBody.js
@@ -0,0 +1,15 @@
+export const TableBody = (props) => {
+  const { characterData, removeCharacter } = props;
+  const rows = characterData.map((row, index) => {
+    return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+          <td>
+            <button onClick={ () => removeCharacter(index) }>Delete</button>
+          </td>
+        </tr>
+    )
+  });
+  return <tbody>{rows}</tbody>
+};
